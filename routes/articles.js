4dd71f79395f40f9da7b2d5b9ab4196e2d9d27dc
Ankush Hegde <59443849+ankush07261@users.noreply.github.com>
2023-10-18 20:34:55 +0530

    Update articles.js

--- routes/articles.js
+++ routes/articles.js
@@ -6,6 +6,12 @@ router.get('/new', function (req, res) {
     res.render('articles/new',{article: new Article() })
 })
 
+router.get('/:id', async (req, res) => {
+    const article = await Article.findById(req.params.id)
+    if (article == null) res.redirect('/')
+    res.render('articles/show', {article: article})
+})
+
 router.post('/', async (req, res) => {
     let article = new Article({
         title: req.body.title,
